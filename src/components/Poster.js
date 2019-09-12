97775a86097ccd48e28b989cc3276bfbd721b5a8
Ayush James <ayush.james1996@gmail.com>
2019-09-13 08:23:20 +1000

    Basic Editor Toolkit setup

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -4,12 +4,15 @@ import htmlToImage from 'html-to-image';
 
 class Poster extends Component {
   state = {
-    posterImage: this.props.location.state.imageURL,
-    posterQuote: this.props.location.state.quote,
-    posterAuthor: this.props.location.state.author,
-    posterColor: "",
-    posterFont: "50px",
-    posterFontSize: "",
+    posterImage: "https://cdn.pixabay.com/photo/2019/03/04/13/28/zebra-4034126_1280.jpg",
+    posterQuote: "Not All Who Wonder Are Lost",
+    posterQuoteSize: 75,
+    posterQuoteColor: undefined,
+    posterQuoteFont: undefined,
+    posterAuthor: "xBlade",
+    posterAuthorSize: 18,
+    posterAuthorColor: undefined,
+    posterAuthorFont: undefined,
   }
 
   // componentDidMount () {
@@ -28,31 +31,42 @@ class Poster extends Component {
       posterQuote: e.target.value,
     });
   }
+  handleChangeQuoteSize = (e) => {
+    this.setState({
+      posterQuoteSize: e.target.value,
+    });
+  }
+  handleChangeQuoteColor = (e) => {
+    this.setState({
+      posterQuoteColor: e.target.value,
+    });
+  }
+  handleChangeQuoteFont = (e) => {
+    this.setState({
+      posterQuoteFont: e.target.value,
+    });
+  }
 
   handleChangeAuthor = (e) => {
     this.setState({
       posterAuthor: e.target.value,
     });
   }
-
-  handleChangeColor = (e) => {
+  handleChangeAuthorSize = (e) => {
     this.setState({
-      posterColor: e.target.value,
+      posterAuthorSize: e.target.value,
     });
   }
-
-  handleChangeFont = (e) => {
+  handleChangeAuthorColor = (e) => {
     this.setState({
-      posterFont: e.target.value,
+      posterAuthorColor: e.target.value,
     });
   }
-
-  handleChangeFontSize = (e) => {
+  handleChangeAuthorFont = (e) => {
     this.setState({
-      posterFontSize: e.target.value,
+      posterAuthorFont: e.target.value,
     });
   }
-
 
   handleClick = () => {
     htmlToImage.toJpeg(document.getElementById('capture'), { quality: 0.95 })
@@ -72,8 +86,8 @@ class Poster extends Component {
           <div id="capture" className="ind_Poster_card card bg-dark text-white rounded-0">
             <img style={{filter: "blur(3px)"}} className="card-img" src={this.state.posterImage}/>
             <div className="card-img-overlay">
-              <h5 style={{color: this.state.posterColor, fontFamily: this.state.posterFont, fontSize: `${this.state.posterFontSize}px`}} className="poster_quote card-title">{this.state.posterQuote}</h5>
-              {this.state.posterAuthor && <p className="poster_author card-text font-weight-light font-italic align-middle">- {this.state.posterAuthor}</p>}
+              <h5 style={{color: this.state.posterQuoteColor, fontFamily: this.state.posterQuoteFont, fontSize: `${this.state.posterQuoteSize}px`}} className="poster_quote card-title">{this.state.posterQuote}</h5>
+              {this.state.posterAuthor && <p style={{color: this.state.posterAuthorColor, fontFamily: this.state.posterAuthorFont, fontSize: `${this.state.posterAuthorSize}px`}} className="poster_author card-text font-weight-light font-italic align-middle">- {this.state.posterAuthor}</p>}
             </div>
           </div>
         </div>
@@ -82,13 +96,14 @@ class Poster extends Component {
           <Link to="/">
           <button style={{marginTop : 8}} className="form__button_white" >Go Back</button>
           </Link>
+
           <div></div>
           <div class="dropdown-divider"></div>
             <div style={{marginLeft: 8, color: "silver"}}>Quote:</div>
             <input style={{marginTop : 3}} className="form__input_black" type="textarea" placeholder="Quote" value={this.state.posterQuote} onChange={this.handleChangeQuote} />
-            <input className="form__input_number " type="number" placeholder="40" onChange={this.handleChangeFontSize}/>
-            <input style={{marginTop : 8, marginLeft: 8, width: 40}} type="color" onChange={this.handleChangeColor}/>
-            <select className="font_picker" onChange={this.handleChangeFont}>
+            <input className="form__input_number " type="number" placeholder="75" onChange={this.handleChangeQuoteSize}/>
+            <input style={{marginTop : 8, marginLeft: 8, width: 40}} type="color" onChange={this.handleChangeQuoteColor}/>
+            <select className="font_picker" onChange={this.handleChangeQuoteFont}>
               <option value="Helvetica Neue">Helvetica Neue</option>
               <option value="Sans-Serif">Sans-serif</option>
               <option value="Monospace">Monospace</option>
@@ -99,9 +114,9 @@ class Poster extends Component {
           <div class="dropdown-divider"></div>
             <div style={{marginLeft: 8, color: "silver"}}>Author:</div>
             <input style={{marginTop : 3}} className="form__input_black" type="text" placeholder="Author" value={this.state.posterAuthor} onChange={this.handleChangeAuthor} />
-            <input className="form__input_number " type="number" placeholder="18" onChange={this.handleChangeFontSize}/>
-            <input style={{marginTop : 8, marginLeft: 8, width: 40}} type="color" onChange={this.handleChangeColor}/>
-            <select className="font_picker" onChange={this.handleChangeFont}>
+            <input className="form__input_number " type="number" placeholder="18" onChange={this.handleChangeAuthorSize}/>
+            <input style={{marginTop : 8, marginLeft: 8, width: 40}} type="color" onChange={this.handleChangeAuthorColor}/>
+            <select className="font_picker" onChange={this.handleChangeAuthorFont}>
               <option value="Helvetica Neue">Helvetica Neue</option>
               <option value="Sans-Serif">Sans-serif</option>
               <option value="Monospace">Monospace</option>
