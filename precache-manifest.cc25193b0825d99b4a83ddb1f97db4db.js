82203c4f0565b7bbb338076a13fa3a4f09d34a36
Ayush James <ayush.james1996@gmail.com>
2019-09-12 14:56:27 +1000

    Updates

---
+++ precache-manifest.cc25193b0825d99b4a83ddb1f97db4db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78b725193bc16801af287a6758afbc13",
+    "url": "/inspo-app/index.html"
+  },
+  {
+    "revision": "abb9c7dfc21a5abcc94d",
+    "url": "/inspo-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2cd1ea581979e1fd983f",
+    "url": "/inspo-app/static/css/main.5b15a89f.chunk.css"
+  },
+  {
+    "revision": "abb9c7dfc21a5abcc94d",
+    "url": "/inspo-app/static/js/2.fc6eebe0.chunk.js"
+  },
+  {
+    "revision": "2cd1ea581979e1fd983f",
+    "url": "/inspo-app/static/js/main.f3c993b0.chunk.js"
+  },
+  {
+    "revision": "28a76deff2a93b00f08a",
+    "url": "/inspo-app/static/js/runtime~main.7db00cf2.js"
+  }
+]);
