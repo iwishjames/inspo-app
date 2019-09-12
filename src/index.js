26fc00c2ba8824d457dc6f09cca9df5d6a802efc
Ayush James <ayush.james1996@gmail.com>
2019-09-12 15:28:27 +1000

    added hash router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Routes from './components/Router';
+import Routes from './components/Routes';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Routes />, document.getElementById('root'));
